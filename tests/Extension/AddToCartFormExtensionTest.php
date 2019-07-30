669d4652fc6398c20c7c942f0c63555881f3990a
Jason Irish <jirish@dynamicagency.com>
2019-07-29 23:15:14 -0500

    initial tests

---
+++ tests/Extension/AddToCartFormExtensionTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Dynamic\Foxy\Inventory\Test\Extension;
+
+use Dynamic\Foxy\Form\AddToCartForm;
+use Dynamic\Foxy\Inventory\Test\TestOnly\Page\TestProduct;
+use Dynamic\Foxy\Inventory\Test\TestOnly\Page\TestProductController;
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Forms\FieldList;
+
+class AddToCartFormExtensionTest extends SapphireTest
+{
+    /**
+     * @var string
+     */
+    protected static $fixture_file = '../fixtures.yml';
+
+    /**
+     * @var array
+     */
+    protected static $extra_dataobjects = [
+        TestProduct::class,
+    ];
+
+    /**
+     * @var array
+     */
+    protected static $extra_controllers = [
+        TestProductController::class,
+    ];
+
+    /**
+     *
+     */
+    public function testUpdateProductFields()
+    {
+        $object = $this->objFromFixture(TestProduct::class, 'one');
+        $controller = TestProductController::create($object);
+        $form = $controller->AddToCartForm();
+        $fields = $form->Fields();
+        $this->assertInstanceOf(FieldList::class, $fields);
+
+        // todo: add assertions to cover CartExpiration and isOutOfStock() checks via fixtures
+    }
+
+    /**
+     *
+     */
+    public function testUpdateProductActions()
+    {
+        $object = $this->objFromFixture(TestProduct::class, 'one');
+        $controller = TestProductController::create($object);
+        $form = $controller->AddToCartForm();
+        $fields = $form->Actions();
+        $this->assertInstanceOf(FieldList::class, $fields);
+
+        // todo: add assertions to cover isOutOfStock() check via fixtures
+    }
+
+    /**
+     *
+     */
+    public function testIsOutOfStock()
+    {
+        $this->markTestSkipped();
+        // todo: write test to test out of stock via fixtures
+    }
+}
